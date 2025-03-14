af2b01cc81a4313bf4b85f65b46192b63d2444dd
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-03-14 04:16:56 +0100

    Add middleware files for authetication and authorisation of roles

---
+++ src/router.ts
@@ -0,0 +1,29 @@
+import express, { Router } from "express";
+
+import userRouter from "./routes/user.route";
+import loginRouter from "./routes/login.route";
+import courseRouter from "./routes/course.route";
+// import moduleRouter from "./routes/module.route";
+import enrollmentRouter from "./routes/enrollment.route";
+import lessonRouter from "./routes/lesson.route";
+// import badgeRouter from "./routes/badge.route";
+import verifyRouter from "./routes/verify.email";
+// import profileRouter from "./routes/profile.route";
+// import certificateRouter from "./routes/certificate.route";
+// import user_badgeRouter from "./routes/user-badge.route";
+
+const router = Router();
+
+router.use("/user", userRouter);
+router.use("/log", loginRouter);
+router.use('/verify', verifyRouter);
+router.use("/course", courseRouter);
+router.use('/lesson', lessonRouter);
+// router.use('/module', moduleRouter);
+router.use('/enrollment', enrollmentRouter);
+// router.use('/badge', badgeRouter);
+// router.use('/profile', profileRouter);
+// router.use('/certificate', certificateRouter);
+// router.use('/user-badge', user_badgeRouter)
+
+export default router;
