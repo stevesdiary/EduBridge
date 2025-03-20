189631dc06ca1fa1c99734fed0410fb6c9f14594
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-03-20 01:54:11 +0100

    Add user_id column to courses table with foreign key reference to users

---
+++ migrations/20250320003834-add-user_id-column-courses.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('courses', 'user_id', {
+      type: Sequelize.UUID,
+      references: {
+        model: 'users',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('courses', 'user_id');
+  }
+};
