eb94f8aaa5fa13cf35cccffd6a2a7198b011f9e0
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-03-23 08:44:06 +0100

    Add aws-sdk dependency; improve error handling in authentication middleware

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -7,20 +7,21 @@ const secret: string = process.env.JWT_SECRET || 'secret';
 
 const authentication = (req: ExpressRequest, res: Response, next: NextFunction) => {
     if (!secret) {
-        throw new Error('JWT_SECRET must be defined in environment variables');
+			throw new Error('JWT_SECRET must be defined in environment variables');
     }
 		let token;
 		// if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
 			token = req.headers.authorization?.split(' ')[1] as string;
 		// }
 		if (!token) {
-				res.status(401).json({ message: 'No token provided' });
-			}
+			res.status(401).json({ message: 'No token provided' });
+			return;
+		}
     try {
 			const decoded = jwt.verify(token, secret as string) as JwtPayload;
 			if (!decoded) {
-					res.status(401).json({ message: 'Unauthorized' });
-					return;
+				res.status(401).json({ message: 'Unauthorized' });
+				return;
 			}
 
 			req.user = decoded //as  { id: string; role: UserRole };
@@ -28,7 +29,8 @@ const authentication = (req: ExpressRequest, res: Response, next: NextFunction) 
 
     } catch (error) {
 			console.error('AUTHENTICATION ERROR:', error);
-			res.status(401).send({ error: 'Please authenticate.' });
+			res.status(401).json({ error: 'Please authenticate.' });
+			return;
     }
 };
 export default authentication;
