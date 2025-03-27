4e27cc0bb4e1389c86f9a8a3d793668b547c9e73
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-03-27 12:29:11 +0100

    Rename duration column to category in lessons table and update its properties

--- migrations/20250327101124-add-category-to-lesson.js
+++ migrations/20250327101124-add-category-to-lesson.js
@@ -10,7 +10,8 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
     await queryInterface.addColumn('lessons', 'category', {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
+      defaultValue: null
     })
   },
 
